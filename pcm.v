3257cd36e6c08b101a18cd31355add1c3718107c
Tawfeeq04 <49206239+Tawfeeq04@users.noreply.github.com>
2019-04-03 09:38:33 +0530

    Update pcm.v

--- pcm.v
+++ pcm.v
@@ -4,24 +4,23 @@ input wire clk,
 output reg out);
 	
 	reg [2:0] count;
-	reg [2:0] code;
+	reg [7:0] code;
 	reg [7:0] data;
 
 	always @(posedge clk) begin
-		if(count > 7) begin
+		if(count > 255) begin
 			count <= 0;
-			code <= msg_signal*5;
+			code <= msg_signal;
 		end
 		else begin
 			if(count <= code) begin
 				data [count] <= 1;
-				count <= count+1;
 			end
 			else begin
 				data [count] <= 0;
-				count <= count+1;
 			end
 		end
+		count <= count + 1;
 		out <= data [count];
 	end
 endmodule
